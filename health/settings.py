87bfcbd059678d96973b590d149abd838bdf4da4
SANIDHIYA <56438908+dragonman164@users.noreply.github.com>
2021-05-29 20:56:15 +0530

    Update settings.py

--- health/settings.py
+++ health/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '-7&vgz@z^@gb#qx80*!1r_g!mg2)dx(i1e=ara%it_46hd$u6f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
