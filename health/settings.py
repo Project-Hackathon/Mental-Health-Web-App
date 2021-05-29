da7a135589bd4bb4ae8bc423753464483b0fc0ae
dragonman164 <sanidhiyafirefox123@gmail.com>
2021-05-29 10:43:26 +0530

    Setting for Deployment

--- health/settings.py
+++ health/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'healthymind.User'
+
+django_heroku.settings(locals())
